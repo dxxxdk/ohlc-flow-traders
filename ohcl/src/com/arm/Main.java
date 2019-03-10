d65440549c107c31dca7190463400fc2e73efa1d
Armand <armand.motocescu@gmail.com>
2019-03-10 19:34:46 +0100

    Implement main

--- ohcl/src/com/arm/Main.java
+++ ohcl/src/com/arm/Main.java
@@ -1,8 +1,24 @@
 package com.arm;
 
+import java.util.Scanner;
+
 public class Main {
+    private static final int OHLC_INTERVAL_SECONDS = 300; // 5 minutes
 
     public static void main(String[] args) {
-	// write your code here
+        OrderBook orderBook = new OrderBook();
+        Ohlc ohlc = new Ohlc(OHLC_INTERVAL_SECONDS);
+
+        try (Scanner scanner = new Scanner(System.in)) {
+            while (true) {
+                String inputRecord = scanner.nextLine();
+                Command c = CommandFactory.parse(inputRecord);
+                if (c == null) {
+                    System.out.println("Invalid input record. Exiting...");
+                    break;
+                }
+                c.execute(orderBook, ohlc);
+            }
+        }
     }
 }
