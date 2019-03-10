db03ac9baafe763a69ad262abd5b8d65fbee0dff
Armand <armand.motocescu@gmail.com>
2019-03-10 18:42:43 +0100

    Add OrderBook class

---
+++ ohcl/src/com/arm/OrderBook.java
@@ -0,0 +1,59 @@
+package com.arm;
+
+import java.util.HashMap;
+import java.util.TreeMap;
+
+class OrderBook {
+    private final TreeMap<Integer, TreeMap<Integer, Order>> buyOrders = new TreeMap<>();
+    private final TreeMap<Integer, TreeMap<Integer, Order>> sellOrders = new TreeMap<>();
+    private final HashMap<String, Order> idToOrder = new HashMap<>();
+
+    /**
+     * @return the buy orders in a nested TreeMap (keys: price, time)
+     */
+    TreeMap<Integer, TreeMap<Integer, Order>> getBuyOrders() {
+        return buyOrders;
+    }
+
+    /**
+     * @return the sell orders in a nested TreeMap (keys: price, time)
+     */
+    TreeMap<Integer, TreeMap<Integer, Order>> getSellOrders() {
+        return sellOrders;
+    }
+
+    /**
+     * @return a HashMap from order IDs to Order instances
+     */
+    HashMap<String, Order> getIdToOrder() {
+        return idToOrder;
+    }
+
+    /**
+     * @return whether there is a bid and ask price available
+     */
+    boolean hasBidAndAsk() {
+        return buyOrders.size() > 0 && sellOrders.size() > 0;
+    }
+
+    /**
+     * @return whether a transaction is possible
+     */
+    boolean isTransactionPossible() {
+        return buyOrders.lastKey() >= sellOrders.firstKey();
+    }
+
+    /**
+     * @return the bid price
+     */
+    int getBid() {
+        return buyOrders.lastEntry().getKey();
+    }
+
+    /**
+     * @return the ask price
+     */
+    int getAsk() {
+        return sellOrders.firstEntry().getKey();
+    }
+}
